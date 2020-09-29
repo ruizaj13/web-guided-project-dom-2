a94cf996aa24604a495886a71f36e1a6e35150a7
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-09-29 19:27:22 +0200

    wip

--- src/index.js
+++ src/index.js
@@ -68,7 +68,10 @@ confirmButton.addEventListener('click', (event) => {
 // 👉 TASK 5- Create a function to cancel the launch.
 // It should close the modal and display a failure report.
 // Add it as a listener for clicks on the cancellation button.
-
+cancelButton.addEventListener('click', (event) => {
+  modal.classList.add('off');
+  failureMessage.classList.remove('off')
+})
 
 // 👉 TASK 6- Create a function that closes the modal if
 // the user hits the Escape key on their keyboard.
